01bddc5c060cc9175e3a4bee4929aac0b04d4e6d
barch <gemg2019@gmail.com>
2024-04-14 11:27:37 +0300

    added absolute path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
+  // ... other configurations
+});
